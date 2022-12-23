3df2502ada7cb61b6b5593f28ace091560ea9f24
Daniel Riedl <daniel.riedl@comsysto.com>
2022-12-23 16:41:43 +0100

    Replace bycrypt with bcryptjs

--- app/server/repository/user.repository.ts
+++ app/server/repository/user.repository.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import type { User } from "~/shared/user";
 import { prisma } from "./prisma.db";
 
--- seed/seed.ts
+++ seed/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import * as dotenv from "dotenv";
 dotenv.config();
 const prisma = new PrismaClient();
